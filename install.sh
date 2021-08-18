b1373c87425fd30f87d5e6475fa87d8e1c03396b
abstractor84 <89123291+abstractor84@users.noreply.github.com>
2021-08-18 11:56:52 +0530

    Adding install.sh to ease installation

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+
+if [[ "$1" == "install" ]]
+then
+    echo "Installing network-wrapper"
+    cp -rf ./network-wrapper ./dhcp-garbagecollection.sh /usr/bin/
+    chmod +x /usr/bin/dhcp-garbagecollection.sh /usr/bin/network-wrapper
+elif [[ "$1" == "uninstall" ]]
+then
+    echo "Uninstalling network-wrapper"
+    rm -f /usr/bin/dhcp-garbagecollection.sh /usr/bin/network-wrapper
+else
+    echo "Usage:"
+    echo "     $0 install => Install network-wrapper"
+    echo "     $0 uninstall => Uninstall network-wrapper"
+fi
